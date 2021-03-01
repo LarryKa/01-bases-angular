4f9fa5d35982723716887298b5082d973970d10c
Your Name <you@example.com>
2021-02-28 18:00:59 -0600

    Avance

---
+++ src/app/dbz/main-page/main-page.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { Personaje } from './../interfaces/dbz.interface';
+@Component({
+  selector: 'app-main-page',
+  templateUrl: './main-page.component.html',
+  styleUrls: ['./main-page.component.css']
+})
+export class MainPageComponent {
+  public personajes: Personaje[] = [
+      {
+        nombre: 'Goku',
+        poder: 15000
+      },
+      {
+        nombre: 'Vegeta',
+        poder: 14000
+      }
+  ];
+
+  nuevo: Personaje = {
+    nombre: 'Bulma',
+    poder: 30
+  }
+
+}
