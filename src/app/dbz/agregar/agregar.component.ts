d796311aba503c2c72d3741ea10081300687ee58
Your Name <you@example.com>
2021-03-07 18:14:06 -0600

    final

--- src/app/dbz/agregar/agregar.component.ts
+++ src/app/dbz/agregar/agregar.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { DbzService } from '../services/dbz.service';
 import { Personaje } from './../interfaces/dbz.interface';
 
 @Component({
@@ -7,18 +8,23 @@ import { Personaje } from './../interfaces/dbz.interface';
 })
 export class AgregarComponent {
 
-  @Input('data') personajes: Personaje[] = [];
 
   @Input() nuevo: Personaje = {
     nombre: '',
     poder: 0
   }
+
+  constructor( private dbzService: DbzService ){}
+
+  //@Output() onNuevoPersonaje: EventEmitter<Personaje> = new EventEmitter();
+
   agregar(){
     if( this.nuevo.nombre.trim().length === 0 ){
       return;
     }
-    this.personajes.push( this.nuevo );
-
+    //this.onNuevoPersonaje.emit( this.nuevo );
+    this.dbzService.agregarPersonaje( this.nuevo );
+    
     this.nuevo = {
       nombre: '',
       poder: 0
